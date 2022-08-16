1b40baa6a6010897557c75a897bc97a99a8f7c45
Simran4481 <c0834481@mylambton.ca>
2022-08-15 22:07:35 -0400

    footer added

---
+++ footer.php
@@ -0,0 +1,92 @@
+<div class="footer"  style="padding-top:1px">
+	<div class="left">
+		<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Turpis in eu mi bibendum neque egestas congue quisque. Sem nulla pharetra diam sit amet nisl suscipit adipiscing bibendum. Imperdiet proin fermentum leo vel orci porta non pulvinar neque. Ac turpis ...<br><span onclick="location.href='about-us.php'" style="cursor:pointer">More...</span>
+
+</p>
+		<h3>Mobi<b>Store</b></h3>
+		
+	</div>
+	<div class="center">
+		<ul>
+		<li style="color: #fff; margin-bottom: 30px; text-transform: uppercase; letter-spacing: 1px ;font-weight: bolder">
+			<p>Store Information</p>
+		</li>
+			<li>
+			<p style="text-align: justify">2000 Random Street, MJT 434</p>
+		</li>
+			<li>
+			<p>mobistore@gmail.com</p>
+		</li>
+			<li>
+			<p><a href="tel:+917889251637" style="color:#888">+0987654321</a></p>
+		</li>
+			
+		</ul>
+			<ul>
+		<li style="color: #fff; margin-bottom: 30px; text-transform: uppercase; letter-spacing: 1px ;font-weight: bolder">
+			<p>Customer Care</p>
+		</li>
+			
+			
+			<a href="contact-us.php"><li>
+			<p>Contact us</p>
+		</li></a>
+				
+				<a href="#"><li>
+			<p>Terms and Condition</p>
+		</li></a>	<a href="#"><li>
+			<p>Privacy Policies</p>
+		</li></a>
+		</ul>
+			<ul>
+		<li style="color: #fff; margin-bottom: 30px; text-transform: uppercase; letter-spacing: 1px ;font-weight: bolder">
+			<p>Social</p>
+		</li>
+			
+			<a href="#"><li>
+			<p>Facebook</p>
+		</li></a>
+			<a href="#"><li>
+			<p>Instagram</p>
+		</li></a>
+			
+		</ul>
+		
+	</div>	
+	<div class="right">
+			<ul>
+		<li style="color: #fff; margin-bottom: 30px; text-transform: uppercase; letter-spacing: 1px ;font-weight: bolder">
+			<p  style="color: #fff;">subscribe here</p>
+		</li>
+			<li>
+			<p>Please submit your email to subscribe to our new updates.</p>
+		</li>
+			
+			<li>
+			<form id="newsForm">
+			<div class="input"><input type="text" id="newsEmail"><span style="cursor: pointer" onclick="validateEmail()">SUBMIT</span></div>
+			</form>
+		</li>
+			<li>
+			<p id="newsMessage" style="display:none"></p>
+		</li>
+		
+		</ul>
+		<ul>
+
+		</ul>
+	</div>	
+		
+		<div class="footer-bottom">
+		
+		<div class="footer-menu">
+			
+		
+	
+			<p>&copy; ALL RIGHTS RESERVED QUICK FIX DEMONS 2022-23</p>
+			
+			</div>
+		</div>
+		
+	</div>
+	
