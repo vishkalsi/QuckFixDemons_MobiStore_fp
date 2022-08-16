bba32d03f63704f704ca26ad5d7c7c404ee96c4a
Amanpreet Kaur <C0854967@mylambton.ca>
2022-08-15 21:06:18 -0400

    about us added

---
+++ about-us.php
@@ -0,0 +1,62 @@
+<?php 
+session_start();
+include "sessions-file.php";
+?>
+<!DOCTYPE>
+<html>
+<head>
+<script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+<link href="css/style.css" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet">
+	<link href="css/slider.css" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Raleway" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Lora" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css?family=Archivo+Narrow" rel="stylesheet">
+    <script src="js/script.js"></script>
+    <script src="https://code.jquery.com/jquery-1.12.4.js"></script>
+  <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+	 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+    <meta name="viewport" content="width=device-width,initial-scale=1,maximum-scale=1" />
+    <meta name="description" content="Welcome to MobiStore.com" />
+    <meta name="format-detection" content="telephone=no">
+    <title>About Us - MobiStore.com</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+    <link rel="icon" href="images/rose.png" type="image/png" sizes="16x16">
+	</head>
+	
+<body>
+      <?php include 'header.php';?>
+
+	
+	
+	
+	<div class="gap"></div>
+	<div class="wrapper">
+		<h1 class="page-title">About Us</h1>
+	
+		<div class="main">
+			<div class="subpage">
+				<p><img width="250" class="sub-img" alt="Ambarsar Bazaar" src="images/about-us.png">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Turpis in eu mi bibendum neque egestas congue quisque. Sem nulla pharetra diam sit amet nisl suscipit adipiscing bibendum. Imperdiet proin fermentum leo vel orci porta non pulvinar neque. Ac turpis egestas integer eget. Adipiscing bibendum est ultricies integer quis auctor. Morbi blandit cursus risus at ultrices mi tempus. Amet purus gravida quis blandit. Felis donec et odio pellentesque diam. Mollis aliquam ut porttitor leo a diam sollicitudin tempor id. Blandit turpis cursus in hac habitasse platea. Orci nulla pellentesque dignissim enim sit. Lacus viverra vitae congue eu consequat ac. Purus in massa tempor nec feugiat nisl. Nunc congue nisi vitae suscipit tellus mauris a diam. Ac auctor augue mauris augue neque gravida in. Venenatis tellus in metus vulputate eu scelerisque. Tortor dignissim convallis aenean et tortor at risus viverra. Metus vulputate eu scelerisque felis imperdiet proin fermentum. Quis imperdiet massa tincidunt nunc pulvinar sapien. <br>
+
+                    Amet luctus venenatis lectus magna fringilla urna porttitor rhoncus dolor. Odio ut enim blandit volutpat maecenas volutpat blandit aliquam. Vulputate ut pharetra sit amet aliquam. Amet nulla facilisi morbi tempus. Faucibus pulvinar elementum integer enim neque volutpat ac tincidunt vitae. Suspendisse faucibus interdum posuere lorem ipsum dolor. Tellus integer feugiat scelerisque varius morbi enim nunc faucibus. Vestibulum morbi blandit cursus risus at ultrices mi tempus. Diam maecenas ultricies mi eget mauris pharetra et ultrices neque. Pellentesque nec nam aliquam sem et tortor consequat id porta. Penatibus et magnis dis parturient montes nascetur ridiculus. Lectus proin nibh nisl condimentum id venenatis a condimentum. Risus viverra adipiscing at in tellus integer. Hendrerit gravida rutrum quisque non. Malesuada proin libero nunc consequat. Morbi tristique senectus et netus et malesuada fames.<br>
+
+                    Ultrices mi tempus imperdiet nulla malesuada pellentesque elit. Sagittis orci a scelerisque purus semper eget duis at tellus. A erat nam at lectus urna duis convallis convallis. Urna nec tincidunt praesent semper feugiat nibh sed pulvinar. Risus feugiat in ante metus dictum at tempor commodo. Neque aliquam vestibulum morbi blandit. Turpis egestas pretium aenean pharetra magna ac placerat. Nisi porta lorem mollis aliquam ut. Nam aliquam sem et tortor consequat id porta. Nunc aliquet bibendum enim facilisis gravida.<br>
+
+                    Velit dignissim sodales ut eu sem integer vitae. Tellus molestie nunc non blandit massa. Nec ultrices dui sapien eget mi proin sed libero enim. Odio pellentesque diam volutpat commodo sed egestas egestas fringilla phasellus. Auctor neque vitae tempus quam pellentesque nec. Nisi vitae suscipit tellus mauris a diam maecenas sed. Donec ac odio tempor orci dapibus ultrices in iaculis nunc. Nulla pellentesque dignissim enim sit amet. Erat nam at lectus urna duis convallis convallis tellus. Pellentesque massa placerat duis ultricies lacus. Ultricies tristique nulla aliquet enim tortor at. Mattis enim ut tellus elementum. Risus at ultrices mi tempus imperdiet nulla malesuada. Vestibulum sed arcu non odio euismod lacinia at quis risus. Et molestie ac feugiat sed lectus vestibulum mattis ullamcorper velit. Vestibulum lorem sed risus ultricies tristique nulla aliquet enim.
+                    <br>
+                    Volutpat ac tincidunt vitae semper. Pellentesque diam volutpat commodo sed egestas egestas fringilla. Arcu cursus vitae congue mauris rhoncus. Nulla malesuada pellentesque elit eget gravida cum sociis natoque penatibus. At ultrices mi tempus imperdiet nulla malesuada pellentesque elit. Id faucibus nisl tincidunt eget nullam non nisi est. Bibendum est ultricies integer quis auctor elit. Risus commodo viverra maecenas accumsan lacus vel facilisis volutpat est. A pellentesque sit amet porttitor eget dolor morbi non arcu. Nam aliquam sem et tortor consequat id porta. Mi eget mauris pharetra et ultrices neque. Mattis enim ut tellus elementum sagittis vitae et leo.
+</p>
+<
+			</div>
+		</div>
+
+	</div>
+	
+	<?php include 'footer.php';?>
+	
+	
+	
+</body>
+</html>
